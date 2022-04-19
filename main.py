0b512bbb185342f12656299eb5f58e6da1069c1d
medianaaa <diana.repich09@gmail.com>
2022-04-19 09:43:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class MortgageCalculatorApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, Mortgage Calculator", halign="center")
+
+MortgageCalculatorApp().run()
